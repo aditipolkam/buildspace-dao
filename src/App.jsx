fb67b9bb39ae55a32b280cd9af70837238a8c014
aditipolkam <aditipolkam@gmail.com>
2023-01-30 09:51:04 +0530

    [ADD] wallet connect feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
 const App = () => {
+  const address = useAddress();
+  console.log("Address:", address)
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to QBDAO</h1>
+        <div className="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
